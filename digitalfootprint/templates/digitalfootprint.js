43f7eb738c5b507a5c97561388d06608b3fd0848
kedron-abbott <kedronabbott@Kedrons-MacBook-Pro.local>
2016-09-25 00:17:42 -0400

    I'm just committing so that I can pull the other stuff

--- digitalfootprint/templates/digitalfootprint.js
+++ digitalfootprint/templates/digitalfootprint.js
@@ -4,28 +4,24 @@ $(document).ready(function() {
 		function() {
 	    $( this ).stop().animate({"opacity": "0"}, "fast"); // this makes the black and white image look like it disapears. (i know i spelled disappears wrong, but i dont care to change it rn)
 			currentBackground = (currentBackground +1)%4;
-    	$('body').addClass("bg-"+currentBackground);
+			$('body').addClass("bg-"+currentBackground);
 			console.log("fading out: ");
 		},
 		function() {
 	    if (!$(this).data("clicked")) { //this part checks to see if the value of clicked is false (basically the either never clicked or they unclicked) and if the value is that way, it makes the black and white fade back in.
 			    $( this ).stop().animate({"opacity": "1"}, "fast");
-					$('body').removeClass("bg-0 bg-1 bg-2 bg-3");
+					$('body').removeClass("bg-0 bg-1 bg-2 bg-3 ");
 	      	console.log("fading in: ");
 	     }
 		}
 	);
-
-
-
-
 
 	$('img.a').on("click", sayhi);
 	function sayhi () {$( this ).data("clicked",!$(this).data("clicked"));
 	}
  //this checks to see if the value of clicked is true. If the value is true, and the image is clicked, it sets to false. If it's false and the image is clicked the value changes to true
 
-	// var selectedFilters = []; //this is the array where the clicked filter names are stored
+	var selectedFilters = []; //this is the array where the clicked filter names are stored
 
 	$('#filterSubmit').on("click", runThroughAllImgs);//this entire function runs through all the images to check and see which ones are clicked v which ones are not
 	function runThroughAllImgs () {
